2589bf9b8620907278d193d605e7059dc972974a
jack0975149 <jack27900240@gmail.com>
2017-11-24 15:57:09 +0800

    promble3

--- WindowsFormsApplication1/WindowsFormsApplication1/Class2.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Class2.cs
@@ -8,5 +8,26 @@ namespace WindowsFormsApplication1
 {
     class Class2
     {
+        public bool IsPrime(int n)
+        {
+
+
+            if (n < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < n; ++i)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
     }
+
 }
